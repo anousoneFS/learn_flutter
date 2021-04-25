8fface53018ca8ede3547392cdf97f26e67f5b3b
anousoneFS <sone.freestyle7898@gmail.com>
2021-04-25 14:25:38 +0700

    fix appbar for ios

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Personal Expenses',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.amber,
